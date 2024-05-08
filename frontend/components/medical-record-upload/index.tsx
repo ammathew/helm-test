f58f7d0e06101af0db2a810fc12f1569caf83457
AnnMary Mathew <amm@AnnMarys-MacBook-Pro.local>
2024-05-08 15:44:37 -0400

    Import useState and FaSpinner. Display upload UI upon clicking 'Simulate Medical Record Upload' button.

--- frontend/components/medical-record-upload/index.tsx
+++ frontend/components/medical-record-upload/index.tsx
@@ -1,18 +1,25 @@
 "use client";
 
+import { useState } from 'react';
 import { useDashboard } from "@/context/dashboard-context";
 import classNames from "classnames";
-import { FaCheck } from "react-icons/fa";
+import { FaCheck, FaSpinner } from "react-icons/fa";
 
 export default function MedicalRecordUpload() {
     const { medicalRecord, setMedicalRecord } = useDashboard();
+    const [isUploading, setIsUploading] = useState(false);
 
     const handleClick = () => {
         setMedicalRecord({ url: "/assets/medical-record.pdf" });
     }
 
     return(
-        <div className="w-1/2 h-64 border border-4 border-gray-200 border-dashed rounded flex flex-row items-center justify-center">
+        <div className="w-1/2 h-64 border border-4 border-gray-200 border-dashed rounded flex flex-row items-center justify-center relative">
+            {isUploading && (
+            <div className="absolute inset-0 bg-gray-200 opacity-50 flex items-center justify-center">
+                <FaSpinner className="animate-spin text-blue-500 text-8xl" />
+            </div>
+            )}
             <button
                 className={classNames(
                     "text-white font-medium py-2 px-4 rounded border border-2",
