f455d9e7aa216206ca5b3ec0a413d8ebba01e7a1
AnnMary Mathew <amm@AnnMarys-MacBook-Pro.local>
2024-05-08 20:25:40 -0400

    Add Case model and new thread which updates case on an interval. Updates case from json files at each interval.

--- backend/main.py
+++ backend/main.py
@@ -8,6 +8,7 @@ import uuid
 import threading
 import time
 
+import json
 
 app = FastAPI()
 
@@ -19,38 +20,57 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+cases = {}
+
 class Case(BaseModel):
     id: str
     created_at: datetime
     status: str
+    summary: Optional[str] = None
+    steps: Optional[List[str]] = None
+
+
+def load_json(file_path):
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+    return data
+
+def update_case_from_json(case: Case, file_path: str):
+    response = load_json(file_path)
+    case.status = response['status']
+    case.summary = response['summary']
+    case.steps = response['steps']
 
 def process_case(case: Case):
     time.sleep(10)
-    case.status = "processing"
-    case.summary = "Processing case..."
-    time.sleep(20)
-    case.status = "complete"
-    case.summary = "Case processed successfully"
-    case.steps = ["Step 1", "Step 2", "Step 3"]
+    update_case_from_json(case, '../assets/response-1.json')
+    print(case)
+    cases[case.id] = case
+    time.sleep(10)
+    update_case_from_json(case, '../assets/response-2.json')
+    print(case)
+    cases[case.id] = case
+    time.sleep(30)
+    update_case_from_json(case, '../assets/response-3.json')
+    print(case)
+    cases[case.id] = case
 
-cases = []
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-
 @app.post("/cases", response_model=Case)
 async def create_case():
-    case_id = str(uuid.uuid4())
+    case_id = '891a_6fbl_87d1_4326'
     case = Case(id=case_id, created_at=datetime.now(), status="submitted")
-    cases.append(case)
+    cases[id] = case
     threading.Thread(target=process_case, args=(case,)).start()
     return case
 
 @app.get("/cases", response_model=List[Case])
 async def get_cases():
-    return cases
+    return list(cases.values())
 
 @app.get("/cases/{case_id}", response_model=Case)
 async def get_case(case_id: str):
